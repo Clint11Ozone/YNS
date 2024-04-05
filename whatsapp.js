e27dbe019625f9aa6e5a9f39630f6a58ec96f518
Clint <Siphiwe19375@gmail.com>
2024-04-05 19:29:42 +0200

    try again

--- whatsapp.js
+++ whatsapp.js
@@ -6,7 +6,13 @@ const {
 } = require("./conversationState");
 const context = require("./context");
 
-const client = new Client({ authStrategy: new LocalAuth({ dataPath: "sessions", }), webVersionCache: { type: 'remote', remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html', } });
+const client = new Client({
+  authStrategy: new LocalAuth({ dataPath: "sessions" }),
+  webVersionCache: {
+    type: 'remote',
+    remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+  }
+});
 
 client.initialize();
 
