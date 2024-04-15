77e69628d7bf4b1d7773aa119af75584b68ea462
Clint <Siphiwe19375@gmail.com>
2024-04-15 10:42:50 +0200

    Revert "fixed?"
    This reverts commit cc726d94a3861435dbb7dc1472de925fff34c937.

--- whatsapp.js
+++ whatsapp.js
@@ -1,4 +1,4 @@
-const { Client, NoAuth } = require("whatsapp-web.js");
+const { Client, NoAuth, LocalAuth } = require("whatsapp-web.js");
 const qrcode = require("qrcode");
 const {
   getConversationState,
@@ -7,12 +7,13 @@ const {
 const context = require("./context");
 
 const client = new Client({
-  webVersionCache: 
-  {
-    remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2402.5-beta.html',
-    type: 'remote' 
-  } 
-})
+  authStrategy: new LocalAuth({ dataPath: "sessions" }),
+  webVersionCache: {
+    type: 'remote',
+    remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+  }
+});
+
 client.initialize();
 
 async function generateQRCode() {
